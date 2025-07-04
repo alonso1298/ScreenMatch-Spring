bbf24bbd0af9a61d6802dd88b2e35bedfd6a25b2
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-07-04 04:31:33 -0600

    add record

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
@@ -16,10 +18,12 @@ public class ScreenmatchApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		ConsumoAPI consumoAPI = new ConsumoAPI();
-		// String json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=malcolm+in+the+middle&apikey=517489d5");
 		String json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=malcolm+in+the+middle&apikey=517489d5");
-		
-		System.out.println(json);
+		// String json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=malcolm+in+the+middle&apikey=517489d5");
+		// System.out.println(json);
+		ConvierteDatos conversor = new ConvierteDatos();
+		DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
 	}
 
 }
--- screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
@@ -1,10 +1,12 @@
 package com.aluracursos.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true) // Va a ignorar aquellos campos que no mapeamos dentro de la clase
 public record DatosSerie(
     @JsonAlias("Title") String titulo, // @JsonAlias("Title") espesifica que en la API viene como Title y solo permite leer 
-    @JsonAlias("otalSeasons") Integer totalTemporadas, 
+    @JsonAlias("totalSeasons") Integer totalTemporadas, 
     @JsonAlias("imdbRating") String evaluacion)
     { 
 
