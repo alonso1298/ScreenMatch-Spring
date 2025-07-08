1b4ddd6bc77bcbe78bf47affaa3307a05ac51e4d
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-07-08 00:20:41 -0600

    add constants

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -2,12 +2,18 @@ package com.aluracursos.screenmatch.principal;
 
 import java.util.Scanner;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private final String URL_BASE = "http://www.omdbapi.com/?t="; // final indica que es una constante
+    private final String API_KEY = "&apikey=517489d5";
 
     public void muestraElMenu(){
         System.out.println("Porfavor escribe el nombre de la serie que deseas buscar");
         String nombreSerie = teclado.nextLine();
+		String json = consumoAPI.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
     }
 
 }
