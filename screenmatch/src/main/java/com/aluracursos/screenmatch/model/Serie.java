38a3266f6954336790cbb54797f60d52dbdec3be
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-07-17 05:11:17 -0600

    Add fetch

--- screenmatch/src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,7 +32,7 @@ public class Serie {
     private Categoria genero;
     private String actores;
     private String sinopsis;
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios;
 
     public Serie(){} // Tenemos que agregar un constructor manualmente por exigencia de JPA
