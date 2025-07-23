d668b91a4f1d53da666d8d8ffc777565a31a61be
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-07-22 22:11:37 -0600

    Devolviendo series en el navegador

--- screenmatch/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,13 +1,21 @@
 package com.aluracursos.screenmatch.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.respository.SerieRepository;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository repository;
 
     @GetMapping("/series")
-    public String mostrarMensaje(){
-        return "Este es mi primer mensje en mi API web";
+    public List<Serie> obtenerTodasLasSeries(){
+        return repository.findAll();
     }
 }
