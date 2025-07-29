13e61ff6cf5343601b4573e9ac5fc25124cdbec5
Alonso Sagrero <55163474+alonso1298@users.noreply.github.com>
2025-07-29 03:36:34 -0600

    Refactor

--- screenmatch/src/main/java/com/aluracursos/screenmatch/CommaLineRunner.java
+++
@@ -1,5 +0,0 @@
-package com.aluracursos.screenmatch;
-
-public interface CommaLineRunner {
-
-}
--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationConsola.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationConsola.java
@@ -1,28 +1,28 @@
-// package com.aluracursos.screenmatch;
+package com.aluracursos.screenmatch;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// // import com.aluracursos.screenmatch.principal.EjemploStreams;
-// import com.aluracursos.screenmatch.principal.Principal;
-// import com.aluracursos.screenmatch.respository.SerieRepository;
+// import com.aluracursos.screenmatch.principal.EjemploStreams;
+import com.aluracursos.screenmatch.principal.Principal;
+import com.aluracursos.screenmatch.respository.SerieRepository;
 
-// @SpringBootApplication
-// public class ScreenmatchApplicationConsola implements CommandLineRunner{
+@SpringBootApplication
+public class ScreenmatchApplicationConsola implements CommandLineRunner{
 
-// 	@Autowired
-// 	private SerieRepository repository;
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(ScreenmatchApplication.class, args);
-// 	}
+	@Autowired
+	private SerieRepository repository;
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
 
-// 	@Override
-// 	public void run(String... args) throws Exception {
-// 		Principal principal = new Principal(repository);
-// 		principal.muestraElMenu();
-// 		// EjemploStreams ejemploStreams = new EjemploStreams();
-// 		// ejemploStreams.muestraEjemplo();
-// 	}
-// }
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repository);
+		principal.muestraElMenu();
+		// EjemploStreams ejemploStreams = new EjemploStreams();
+		// ejemploStreams.muestraEjemplo();
+	}
+}
